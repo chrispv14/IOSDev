fe7d0e421733b5f4b32428122bea16b4e9b115d0
stephen-cr <stephencrout03@gmail.com>
2025-05-10 11:27:09 +1000

    Update ContentView.swift
    Added ability to delete bookings from My Bookings view by swiping

--- IOSDev/ContentView.swift
+++ IOSDev/ContentView.swift
@@ -79,16 +79,23 @@ struct MyBookingsView: View {
     
     var body: some View {
         NavigationView {
-            List(bookingStore.bookings) { booking in
-                VStack(alignment: .leading) {
-                    Text("\(booking.vehicle.brand) \(booking.vehicle.model)")
-                        .font(.headline)
-                    Text("Pickup: \(booking.pickupDate, style: .date)")
-                    Text("Return: \(booking.returnDate, style: .date)")
+            List {
+                ForEach(bookingStore.bookings) { booking in
+                    VStack(alignment: .leading) {
+                        Text("\(booking.vehicle.brand) \(booking.vehicle.model)")
+                            .font(.headline)
+                        Text("Pickup: \(booking.pickupDate, style: .date)")
+                        Text("Return: \(booking.returnDate, style: .date)")
+                    }
                 }
+                .onDelete(perform: deleteBooking)
             }
             .navigationTitle(Text("My Bookings"))
         }
+    }
+    
+    private func deleteBooking(at offsets: IndexSet) {
+        bookingStore.bookings.remove(atOffsets: offsets)
     }
 }
 
